f6575eef985adc7d72f7637f2fea232225fa5319
mikol <mikolajek.a@gmail.com>
2023-10-20 14:53:01 +0200

    MealItem widget improved (161)

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/main.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -10,18 +11,22 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget content =
-        ListView.builder(itemCount: meals.length, itemBuilder: (ctx, index) => Text(meals[index].title));
+    Widget content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
 
     if (meals.isEmpty) {
       content = Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text('Uh oh...nothing here!', style: Theme.of(context)
-                .textTheme
-                .headlineLarge!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),),
+            Text(
+              'Uh oh...nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
             const SizedBox(
               height: 16,
             ),
